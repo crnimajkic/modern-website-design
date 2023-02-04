f400d09965e484b25a171787063a9ec6ab5dfb9c
crnimajkic <mlmajkicmlmajkic@gmail.com>
2023-02-04 17:04:40 +0100

    first commit

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./article.css"
+
+
+const Article = ({ imgUrl, text, date }) => {
+  return (
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt="articleImg" />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+
+        <p>Read full Article</p>
+      </div>
+    </div>
+  )
+}
+
+export default Article
---
+++ src/components/cta/Cta.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './cta.css'
+
+const Cta = () => {
+  return (
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p> Request Early Access to Get started</p>
+        <h3>Register today & start exploring the endles possibilities</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button>Get Started</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cta
